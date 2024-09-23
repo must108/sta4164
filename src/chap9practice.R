c60cd6c10fb980e4e25164dbaa187038fd3b66aa
must108 <136145181+must108@users.noreply.github.com>
2024-09-23 19:46:19 -0400

    r practicwe

---
+++ src/chap9practice.R
@@ -0,0 +1,10 @@
+# import ISLR library
+library(ISLR)
+
+attach(College)
+?College
+
+# all plots have been saved
+# plot(Grad.Rate, Top10perc)
+# plot(Grad.Rate, Outstate)
+# plot(Grad.Rate, perc.alumni)
