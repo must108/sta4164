086436aa1aaa3f41e56f0239451a0bae9b2def2a
must108 <136145181+must108@users.noreply.github.com>
2024-10-07 19:41:13 -0400

    yes

--- projects/projectA.R
+++ projects/projectA.R
@@ -2,4 +2,17 @@
 
 # part A
 dataset <- read.csv("./data/heart_failure_clinical_records_dataset.csv")
-print(dataset)
+df <- data.frame(dataset)
+attach(df)
+print(df)
+
+# part B
+plot(age, serum_creatinine,
+  main = "Age vs Serum Creatinine",
+  xlab = "Age",
+  ylab = "Serum Creatinine"
+)
+
+# part C
+heart_model <- lm(serum_creatinine ~ age, data = df)
+print(summary(heart_model))
