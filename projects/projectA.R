0a74057420050bd5ba140e5f4d69d497c5093e19
must108 <mustaeen18@gmail.com>
2024-10-08 01:40:26 -0400

    lol this assignment is like 2 days late

--- projects/projectA.R
+++ projects/projectA.R
@@ -13,6 +13,26 @@ plot(age, serum_creatinine,
   ylab = "Serum Creatinine"
 )
 
-# part C
+# part C/D/E
 heart_model <- lm(serum_creatinine ~ age, data = df)
 print(summary(heart_model))
+
+# part F
+correl_heart <- cor.test(age, serum_creatinine)
+print(correl_heart$conf.int)
+
+# part G
+qqnorm(residuals(heart_model), main = "Q-Q Plot")
+qqline(residuals(heart_model), col = "red")
+
+# part H
+model_log <- lm(log(serum_creatinine) ~ age, data = df)
+model_sqrt <- lm(sqrt(serum_creatinine) ~ age, data = df)
+print(summary(model_log))
+print(summary(model_sqrt))
+
+# par(mfrow = c(2, 2))
+# plot(model_log)
+
+par(mfrow = c(2, 2))
+plot(model_sqrt)
