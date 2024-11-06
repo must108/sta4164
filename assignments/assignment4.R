6aea8d9a99e35874cc6be39013c621710b6d3d7b
must108 <mustaeen18@gmail.com>
2024-11-05 23:32:24 -0500

    completed assignment 4

--- assignments/assignment4.R
+++ assignments/assignment4.R
@@ -20,3 +20,19 @@ print(dim(df))
 # part C
 model <- lm(Grad.Rate ~ S.F.Ratio + Terminal + Personal, data = df)
 avPlots(model)
+
+# part D
+print("Part D")
+initial_model <- lm(Grad.Rate ~ S.F.Ratio * Personal + Terminal, data = df)
+
+final_model <- ols_step_backward_aic(initial_model)
+print(final_model)
+
+# part E
+final_formula <- final_model$model
+refitted_model <- lm(final_formula, data = df)
+
+preds <- predict(refitted_model, df)
+
+mae_value <- mae(df$Grad.Rate, preds)
+print(mae_value)
